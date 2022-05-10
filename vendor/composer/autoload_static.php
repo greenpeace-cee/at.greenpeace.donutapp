02da2d3154e010c538fea79c66b16e48dfdc8e99
Patrick Figel <pfigel@greenpeace.org>
2022-05-10 12:18:27 +0200

    Update depepdencies

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,8 +8,11 @@ class ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8
 {
     public static $files = array (
         '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
+        '25072dd6e2470089de65ae7bf11d3109' => __DIR__ . '/..' . '/symfony/polyfill-php72/bootstrap.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        'f598d06aa772fa33d905e87be6398fb1' => __DIR__ . '/..' . '/symfony/polyfill-intl-idn/bootstrap.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
@@ -17,6 +20,12 @@ class ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8
         'T' => 
         array (
             'Tdely\\Luhn\\' => 11,
+        ),
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Php72\\' => 23,
+            'Symfony\\Polyfill\\Intl\\Normalizer\\' => 33,
+            'Symfony\\Polyfill\\Intl\\Idn\\' => 26,
         ),
         'P' => 
         array (
@@ -34,6 +43,18 @@ class ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8
         'Tdely\\Luhn\\' => 
         array (
             0 => __DIR__ . '/..' . '/tdely/luhn/src',
+        ),
+        'Symfony\\Polyfill\\Php72\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php72',
+        ),
+        'Symfony\\Polyfill\\Intl\\Normalizer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer',
+        ),
+        'Symfony\\Polyfill\\Intl\\Idn\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-intl-idn',
         ),
         'Psr\\Http\\Message\\' => 
         array (
@@ -53,11 +74,17 @@ class ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'Normalizer' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit342f99b47f3da81d981f13d4a2c1cfd8::$classMap;
 
         }, null, ClassLoader::class);
     }
